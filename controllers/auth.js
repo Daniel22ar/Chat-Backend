a72d88e3918cbb9f94db60633fdfed4739d56cc2
Desingnet <daniel.arredondo@desingnet.com>
2024-03-27 00:22:11 -0600

    Cambios Servidor autenticacion

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,7 +16,7 @@ const crearUsuario = async (req, res = response ) => {
         if (existeEmail) {
             return res.status(400).json({
                 ok: false,
-                msg: 'Error inesperado'
+                msg: 'El correo ya esta registrado...'
             });
         }   
 
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,20 @@
 const {io} = require('../index');
+const { comprobarJWT } = require('../helpers/jwt');
 
 // Mensajes de sockets
 io.on('connection', client => {
    
-    console.log('Cliente conectado');
+   console.log('Cliente conectado');
+
+   const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
+
+   // Verificar autenticación
+   if (!valido) { return client.disconnect(); }
+   
  
-    client.on('disconnect', () => {
-       console.log('Cliente desconectado');
-    });
+   client.on('disconnect', () => {
+      console.log('Cliente desconectado');
+   });
  
    //  client.on('mensaje', (payload)=> {
    //     console.log('Mensaje recibido!!!', payload);
