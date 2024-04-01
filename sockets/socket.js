417d6c0561d028ddc8d572aa5f528d5ecfcb1e09
Desingnet <daniel.arredondo@desingnet.com>
2024-03-31 20:00:23 -0600

    Actualizacion Sockets

--- sockets/socket.js
+++ sockets/socket.js
@@ -14,6 +14,15 @@ io.on('connection', client => {
 
    // Cliente autenticado
    usuarioConectado(uid);
+
+   // Ingresar al usuario a una sala en particular
+
+   client.join(uid);
+
+   // Escuchar del cliente el mensaje-personal
+   client.on ('mensaje-personal', (payload ) => {
+      io.to(payload.para).emit('mensaje-personal', payload);
+   });
    
  
    client.on('disconnect', () => {
